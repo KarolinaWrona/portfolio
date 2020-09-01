8951c00181db06a690cac1c1b45cb89866e5b5f8
KarolinaWrona <karolinawrona9804@gmail.com>
2020-09-01 19:31:36 +0200

    Updates

---
+++ precache-manifest.f950e6545cc48979f53e923abd0a665e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f7d68a90f91871df3ffba64793322d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "485f7e5220994e9a77c5",
+    "url": "/portfolio/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "048a752b569e63db714c",
+    "url": "/portfolio/static/css/main.3ab050d5.chunk.css"
+  },
+  {
+    "revision": "485f7e5220994e9a77c5",
+    "url": "/portfolio/static/js/2.44da2089.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.44da2089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048a752b569e63db714c",
+    "url": "/portfolio/static/js/main.ca39a235.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d200f312ed34f19edbb9b17fd97326fb",
+    "url": "/portfolio/static/media/bg1.d200f312.jpg"
+  },
+  {
+    "revision": "6981697d78f0a0bdeb9d7008eb292564",
+    "url": "/portfolio/static/media/bg2.6981697d.jpg"
+  }
+]);
